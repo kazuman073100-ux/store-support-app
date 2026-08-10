21e23b5b568ab26ed3686063f4e56820bb0d8371
kazuman073100-ux <kazuman073100@gmail.com>
2026-08-10 15:57:07 +0900

    Connect API to Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setAnswer("通信中...");
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/ask", {
+      const response = await fetch("/api/ask", {
       method: "post",
       headers: {
         "Content-Type": "application/json",
